a3a6a73e3481ed150e7fa7f46fd2e662af474ef1
Shubham Saini <sainisbm888@gmail.com>
2026-09-15 09:55:05 +0530

    Addition of two num in js

---
+++ 18-08-26/addUserInput.js
@@ -0,0 +1,7 @@
+
+function add() {
+    let num1=document.getElementById("t1").value;
+    let num2=document.getElementById("t2").value;
+    let sum=Number(num1)+Number(num2);
+    document.getElementById("b1").innerHTML=sum;
+}
